cd874b9c905a3fc7ebd49fa0848a349c758f867d
TranPhu24 <clone003nhe@gmail.com>
2025-08-03 00:58:04 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, 
+  });
+
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT')
+  const port = configService.get('PORT') || 8080;
   await app.listen(port);
 }
 bootstrap();
